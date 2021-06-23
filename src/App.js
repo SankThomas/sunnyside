2a3f081dce4dde8c179ce1f67ebcf842171ba004
Thomas Sankara <thomassankara74@gmail.com>
2021-06-23 17:35:33 +0300

    render components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import GraphicMobile from "./components/GraphicMobile"
+import GraphicDesktop from "./components/GraphicDesktop"
+import Header from "./components/Header"
+import Showcase from "./components/Showcase"
+import StandOut from "./components/StandOut"
+import Transform from "./components/Transform"
+import Testimonials from "./components/Testimonials"
+import GridMobile from "./components/GridMobile"
+import GridDesktop from "./components/GridDesktop"
+import Footer from "./components/Footer"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Showcase />
+      <Transform />
+      <StandOut />
+      <div className="block md:hidden">
+        <GraphicMobile />
+      </div>
+      <div className="hidden md:block">
+        <GraphicDesktop />
+      </div>
+      <Testimonials />
+      <div className="block md:hidden">
+        <GridMobile />
+      </div>
+      <div className="hidden md:block">
+        <GridDesktop />
+      </div>
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
